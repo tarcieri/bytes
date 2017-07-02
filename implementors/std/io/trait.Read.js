f07ebb9eb4ac520fc51182b5d282cbd91ea54159
Travis CI User <travis@example.org>
2017-07-02 21:24:00 +0000

    Documentation for carllerche/bytes@6750a26

---
+++ implementors/std/io/trait.Read.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["bytes"] = ["impl&lt;B:&nbsp;<a class='trait' href='bytes/buf/trait.Buf.html' title='bytes::buf::Buf'>Buf</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='bytes/buf/struct.Reader.html' title='bytes::buf::Reader'>Reader</a>&lt;B&gt;",];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["bytes"] = ["impl&lt;B:&nbsp;<a class='trait' href='bytes/buf/trait.BufMut.html' title='bytes::buf::BufMut'>BufMut</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/buf/struct.Writer.html' title='bytes::buf::Writer'>Writer</a>&lt;B&gt;",];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
